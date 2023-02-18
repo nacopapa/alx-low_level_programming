2cd87e69c6e4a1efc5bc4647e2044d9d69dbab61
Michael Asare <nacolepsy@gmail.com>
2023-02-18 03:55:40 +0000

    I just added a file  called 0-positive_or_negative.c to determine negative, zero or positive numbers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - The entry part of the program
+ *
+ * Description: A program to return negative and positive numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	if (n == 0)
+	{
+		printf("%d is zero", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,5 @@
+This repository contains codes on 0x01. C - Variables, if, else, while
+
+The program 0-positive_or_negative.c assigns a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.
+
+
