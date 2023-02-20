585323d645d0e82d7ae6ac3b8721f8cac15cd540
Michael Asare <nacolepsy@gmail.com>
2023-02-20 19:51:08 +0000

    I added another file to count double digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -  main entry poin to the program
+ *
+ * Description: A program to count double digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 1;
+
+	while (x < 90)
+	{
+		putchar ((x / 10) + '0');
+		putchar ((x % 10) + '0');
+		if (x < 90)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+}
