e03c35fb567c77e694b8b58131926a84634fdd3c
Michael Asare <nacolepsy@gmail.com>
2023-02-24 21:40:55 +0000

    added a function to multipy 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * mul - a function that multiplies two integers.
+ * @a:int integer type
+ * @b:int integer type
+ * Return: Always (0)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
