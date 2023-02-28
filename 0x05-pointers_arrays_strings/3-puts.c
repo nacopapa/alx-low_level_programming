68077eed72dbd4097e3b921b5047be630fa88a98
Michael Asare <nacolepsy@gmail.com>
2023-02-28 05:30:16 +0000

    added task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts -  prints string
+ * @str: string to be printed
+ */
+ void _puts(char *srt)
+ {
+ 	while (*str != '\0')
+ 	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+ }
