35eccca861e23bf815f66fca7eb8f5e710f18457
Michael Asare <nacolepsy@gmail.com>
2023-02-28 05:36:54 +0000

    update

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
  * _puts -  prints string
  * @str: string to be printed
  */
- void _puts(char *srt)
- {
- 	while (*str != '\0')
- 	{
+void _puts(char *srt)
+{
+	while (*str != '\0')
+	{
 		_putchar(*str++);
 	}
 	_putchar('\n');
- }
+}
