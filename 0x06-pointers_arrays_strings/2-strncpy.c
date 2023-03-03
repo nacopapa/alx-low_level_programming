b1af4badf45d4bbd0765b956b51a8b890d2d3055
Michael Asare <nacolepsy@gmail.com>
2023-03-03 21:39:38 +0000

    made changes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int x;
 
 	for (x = 0; x < n && src[x] != '\0'; x++)
-		dest[i] = srcx[x];
+		dest[x] = srcx[x];
 
 	for (; n > x; x++)
 		dest[x] = '\0';
